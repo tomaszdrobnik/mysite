e062b5aec4b50575415533a67ab14aededf06216
bzalewa <bzalewa@users.noreply.github.com>
2023-03-06 20:54:10 +0100

    Added generic views

--- polls/urls.py
+++ polls/urls.py
@@ -5,13 +5,13 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # ex: /added the word 'survey' instead of polls
     # ex.survey/5/ instead of polls/5
     # the 'name' value as called by the {% url %} template tag
-    path('survey/<int:question_id>/', views.detail, name='detail'),
+    path('survey/<int:pk>/', views.DetailView.as_view(), name='detail'),
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]

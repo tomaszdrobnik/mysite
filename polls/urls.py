402c7ba4a72761cc6b76643e605609e5dded7971
bzalewa <bzalewa@users.noreply.github.com>
2023-03-06 19:01:02 +0100

    Changed url

--- polls/urls.py
+++ polls/urls.py
@@ -5,9 +5,10 @@ from . import views
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
-    # ex: /polls/5/
+    # ex: /added the word 'survey' instead of polls
+    # ex.survey/5/ instead of polls/5
     # the 'name' value as called by the {% url %} template tag
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('survey/<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
